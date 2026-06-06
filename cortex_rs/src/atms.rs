f794befa2f585e0d35014c50358ac96816cb872d
Tester <tester@cortex.com>
2026-06-06 16:02:06 +0200

    feat(telemetry): implement bidirectional websocket for edge sensor ingestion

--- cortex_rs/src/atms.rs
+++ cortex_rs/src/atms.rs
@@ -1,14 +1,13 @@
-use uuid::Uuid;
 use std::collections::{HashMap, HashSet};
 use pyo3::prelude::*;
 
 #[pyclass]
 #[derive(Debug, Clone)]
 pub struct AtmsGraph {
-    pub nodes: HashSet<Uuid>,
-    pub dependencies: HashMap<Uuid, Vec<Uuid>>, // Child -> Parents (DependsOn)
-    pub entails: HashMap<Uuid, Vec<Uuid>>,      // Parent -> Children (Entails)
-    pub conflicts: HashMap<Uuid, Vec<Uuid>>,    // Node -> Conflicting nodes
+    pub nodes: HashSet<String>,
+    pub dependencies: HashMap<String, Vec<String>>, // Child -> Parents (DependsOn)
+    pub entails: HashMap<String, Vec<String>>,      // Parent -> Children (Entails)
+    pub conflicts: HashMap<String, Vec<String>>,    // Node -> Conflicting nodes
 }
 
 #[pymethods]
@@ -24,19 +23,15 @@ impl AtmsGraph {
     }
 
     pub fn add_node(&mut self, node_id_str: &str) -> PyResult<()> {
-        let node_id = Uuid::parse_str(node_id_str)
-            .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
-        self.nodes.insert(node_id);
+        self.nodes.insert(node_id_str.to_string());
         Ok(())
     }
 
     pub fn add_dependency(&mut self, child_str: &str, parent_str: &str) -> PyResult<()> {
-        let child = Uuid::parse_str(child_str)
-            .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
-        let parent = Uuid::parse_str(parent_str)
-            .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
+        let child = child_str.to_string();
+        let parent = parent_str.to_string();
         
-        self.dependencies.entry(child).or_insert_with(Vec::new).push(parent);
+        self.dependencies.entry(child.clone()).or_insert_with(Vec::new).push(parent.clone());
         self.entails.entry(parent).or_insert_with(Vec::new).push(child);
         Ok(())
     }
